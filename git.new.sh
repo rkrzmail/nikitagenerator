4ff23e7814f85833d8fff9c64c40ed49479ce38a
rkrzmail <rkrzmail@gmail.com>
2023-08-07 16:47:05 +0700

    first commit

---
+++ git.new.sh
@@ -0,0 +1,7 @@
+echo "# nikitagenerator" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rkrzmail/nikitagenerator.git
+git push -u origin main
